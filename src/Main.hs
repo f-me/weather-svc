a32be81eefddfab53c57b7c462ac2ebf15ae0e62
Max Taldykin <jorpic@gmail.com>
2016-01-16 14:35:43 +0300

    Simple weather cache

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,18 @@
 
 module Main where
 
-import Data.Text (Text)
-import qualified Data.Double.Conversion.Text as D
+
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Either (left)
-
 import Control.Lens
+
 import Data.Aeson.Lens
+import Data.Text (Text)
+import Data.Monoid ((<>))
+import Data.IORef
+import qualified Data.Map as Map
+import qualified Data.Double.Conversion.Text as D
+import Data.Time (getCurrentTime, diffUTCTime)
 
 import System.Environment (getArgs, getProgName)
 import qualified Data.Configurator as Config
@@ -29,30 +34,53 @@ openWeatherAPI :: String
 openWeatherAPI = "http://api.openweathermap.org/data/2.5/weather"
 
 
+getWeather :: Text -> Text -> Text -> IO (Maybe Double)
+getWeather apiKey lonT latT = do
+  let opts = defaults
+        & (param "units" .~ ["metric"])
+        & (param "lon"   .~ [lonT])
+        & (param "lat"   .~ [latT])
+        & (param "APPID" .~ [apiKey])
+  r <- getWith opts openWeatherAPI
+  return $ r ^? responseBody . key "main" . key "temp" . _Double
+
+
 main :: IO ()
 main = do
   progName <- getProgName
   getArgs >>= \case
     [configFile] -> do
       conf <- Config.load [Config.Required configFile]
-      apiKey     <- Config.require conf "api.key"
-      serverPort <- Config.require conf "server.port"
+      apiKey      <- Config.require conf "api.key"
+      serverPort  <- Config.require conf "server.port"
+      cachePeriod <- (*60) <$> Config.require conf "cache.minutes" :: IO Int
       -- statsdPort <- Config.require conf "statsd.port"
-      -- statsd request
+
+      cache <- newIORef Map.empty
 
       run serverPort
         $ logStdoutDev
         $ serve (Proxy :: Proxy API)
         $ curry $ \case
           (Just lon, Just lat) -> do
-            let opts = defaults
-                  & (param "units" .~ ["metric"])
-                  & (param "lon"   .~ [D.toShortest lon])
-                  & (param "lat"   .~ [D.toShortest lat])
-                  & (param "APPID" .~ [apiKey])
-            r <- liftIO $ getWith opts openWeatherAPI
-            let Just temp = r ^? responseBody . key "main" . key "temp" . _Double
-            return $ D.toShortest temp
+            let lonT = D.toShortest lon
+            let latT = D.toShortest lat
+            let cacheKey = lonT <> ":" <> latT
+
+            curTime <- liftIO getCurrentTime
+
+            cacheMap <- liftIO (readIORef cache)
+            case Map.lookup cacheKey cacheMap of
+              Just (time, temp)
+                | round (diffUTCTime curTime time) < cachePeriod
+                  -> return temp
+
+              _ -> liftIO $ do
+                Just temp <- getWeather apiKey lonT latT
+                let tempT = D.toShortest temp
+                atomicModifyIORef' cache
+                  $ (,()) . Map.insert cacheKey (curTime, tempT)
+                return tempT
 
           _ -> left $ err401 {errBody = "Invalid lon/lat"}
 
